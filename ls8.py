6d2062c186fa6ad897a6fb303886f0a1d34e477e
[Brandon] <desselle.brandon2@gmail.com>
2020-02-28 13:21:12 -0600

    Refactoring ls8.py

--- ls8.py
+++ ls8.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python3
-
-"""Main."""
+"""Main"""
 
 import sys
 import os
-from cpu import *
-
+from cpu import CPU
 
 if len(sys.argv) != 2:
-    print("Usage: Please load a file", file=sys.stderr)
+    print("ERR: No file given:\t python ls8.py examples/file_name")
     sys.exit(1)
 
 script_dir = os.path.dirname(__file__)
-
-# argument should be file path, e.g. ./example/foo.bar
-file_path = os.path.join(script_dir, sys.argv[1])
+file_name = sys.argv[1]
+# arg handed should be a file path ex: examples/file_name
+file_path = os.path.join(script_dir, file_name)
 
 cpu = CPU()
-
-cpu.load(sys.argv[1])
+cpu.load(file_name)
 cpu.run()
